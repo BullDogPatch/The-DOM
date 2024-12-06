6bdbdc0d4a01107b6c0361cac90ac510cedfeb8d
Craig Clayton <craig__clayton@outlook.com>
2024-12-06 09:25:41 +0000

    select the html using js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('test');
+const body = document.querySelector('body');
+console.log(body);
+
+const h1 = document.querySelector('h1');
+console.log(h1);
+
+h1.textContent = 'This is a worksop about the DOM';
+h1.style.backgroundColor = 'lime';
+h1.style.color = 'rebeccapurple';
+
+const image = document.querySelector('img');
+image.src =
+  'https://media.australian.museum/media/dd/images/gold_striped_earwig.width-1200.5a25695.jpg';
+image.alt = 'Pictue of a earwig';
+image.style.borderRadius = '30px';
+image.style.border = '10px dashed black';
